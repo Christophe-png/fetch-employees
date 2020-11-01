ce752c2f7e01a7c5adbec62e09129767f3b6a542
CASTAN <christophecastan4@gmail.com>
2020-11-01 22:12:18 +0100

    rendu conditionnel

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,36 +4,16 @@ import "./App.css";
 import DisplayEmployee from "./components/DisplayEmployee";
 import axios from "axios";
 
-const sampleEmployee = {
-  gender: "male",
-  name: {
-    first: "Charlie",
-    last: "Thompson",
-  },
-  location: {
-    street: {
-      number: 761,
-      name: "Tay Street",
-    },
-    city: "Timaru",
-    postcode: 76111,
-  },
-  email: "charlie.thompson@example.com",
-  picture: {
-    medium: "https://randomuser.me/api/portraits/med/men/40.jpg",
-  },
-};
-
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      employee: sampleEmployee,
-    };
-    this.getEmployee = this.getEmployee.bind(this);
+  state = {
+    employee: null,
+  };
+
+  componentDidMount() {
+    this.getEmployee();
   }
 
-  getEmployee() {
+  getEmployee = () => {
     axios
       .get("https://randomuser.me/api?nat=fr")
       .then((response) => response.data)
@@ -42,11 +22,16 @@ class App extends React.Component {
           employee: data.results[0],
         });
       });
-  }
+  };
+
   render() {
     return (
       <div className="App">
-        <DisplayEmployee employee={this.state.employee} />
+        {this.state.employee ? (
+          <DisplayEmployee employee={this.state.employee} />
+        ) : (
+          <p>No data yet</p>
+        )}
         <button type="button" onClick={this.getEmployee}>
           Get employee
         </button>
